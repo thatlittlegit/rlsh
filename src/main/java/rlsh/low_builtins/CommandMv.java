6d0a0d4e4869d73a3de8d549870345f3ac0cb9be
wapidstyle <wapidstyle@live.ca>
2017-04-14 10:34:53 -0400

    Add Apache Commons IO and make mv use it

--- src/main/java/rlsh/low_builtins/CommandMv.java
+++ src/main/java/rlsh/low_builtins/CommandMv.java
@@ -4,7 +4,12 @@ import rlsh.Command;
 import rlsh.CommandAction;
 
 import java.util.ArrayList;
+
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileExistsException;
+import org.apache.commons.io.FileUtils;
 
 import wapi.core.DataManager;
 
@@ -12,6 +17,27 @@ public class CommandMv extends Command implements CommandAction {
     public CommandMv(ArrayList<String> arguments) {
         super("mv", arguments);
         this.action = this;
+    }
+
+    protected void move(File from, File to) {
+        try {
+            if(from.isDirectory() && to.isDirectory()) {
+                FileUtils.moveDirectoryToDirectory(from, to, true);
+            } else if(from.isDirectory() && !to.isDirectory()) {
+                throw new IOException("Cannot move a directory to a file");
+            } else {
+                FileUtils.moveFile(from, to);
+            }
+        } catch(FileExistsException e) {
+            try {
+                FileUtils.forceDelete(to);
+            } catch(IOException ioe) {
+                System.err.println("rlsh: mv: Error deleting old " + to.getName() + ": " + ioe.getMessage());
+            }
+            move(from, to);
+        } catch(IOException e) {
+            System.err.println("rlsh: mv: Failed to move file, does it exist? (" + e.getMessage() + ")");
+        }
     }
 
     public void run() {
@@ -23,14 +49,10 @@ public class CommandMv extends Command implements CommandAction {
             System.err.println("rlsh: mv: Usage: mv <from> <to>");
         } else {
             if(arguments.get(0).startsWith("/")) {
-                if(!new File(arguments.get(0)).renameTo(new File(arguments.get(1)))) {
-                    System.err.println("rlsh: mv: File not moved, does it exist?");
-                }
+                move(new File(arguments.get(0)), new File(arguments.get(1)));
             } else {
                 String directory = DataManager.get("rlsh", "directory").string;
-                if(!new File(directory + "/" + arguments.get(0)).renameTo(new File(directory + "/" + arguments.get(1)))) {
-                    System.err.println("rlsh: mv: File not moved, does it exist?");
-                }
+                move(new File(directory + "/" + arguments.get(0)), new File(directory + "/" + arguments.get(1)));
             }
         }
     }
