99b520b70d04ae7bdfe27883776b2c6039e2ae0a
wapidstyle <wapidstyle@live.ca>
2017-07-05 09:39:36 -0400

    Add a parser module for separating commands

---
+++ src/main/java/rlsh/parser/ParserModuleSeperator.java
@@ -0,0 +1,30 @@
+package rlsh.parser;
+
+import rlsh.Command;
+import rlsh.CommandParser;
+
+public class ParserModuleSeperator implements ParserModule {
+    public static final String name = "seperator";
+
+    public String getName() {
+	return name;
+    }
+
+    public boolean getIfNeedToRun(Command command) {
+	try {
+	    return command.completeInput.split(";").length > 0;
+	} catch(NullPointerException e) {
+	    return false;
+	}
+    }
+
+    public void run(Command rawCommand) {
+	for(String command : rawCommand.completeInput.split(";")) {
+	    ArrayList<String> arguments = new ArrayList<String>(Arrays.asList(command.split(" ")));
+	    arguments.remove(0);
+	    Command newCommand = new Command(command.split(" ")[0], arguments, null);
+	    rawCommand.completeInput = command;
+	    CommandParser.run(command);
+	}
+    }
+}
